91464489192d04dab3113ff6c7fea16a1e5b4c64
Bry-D <bryandang10@gmail.com>
2021-12-16 17:41:11 -0800

    Implement action bar to add item in to-do list

---
+++ app/src/main/java/app/project/pomolist/ConfigureTaskActivity.java
@@ -0,0 +1,13 @@
+package app.project.pomolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigureTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configure_task);
+    }
+}
--- app/src/main/java/app/project/pomolist/ToDoListActivity.java
+++ app/src/main/java/app/project/pomolist/ToDoListActivity.java
@@ -1,10 +1,20 @@
 package app.project.pomolist;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class ToDoListActivity extends AppCompatActivity {
 
@@ -12,9 +22,60 @@ public class ToDoListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+
+        setActionBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_to_do_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_to_do_list) {
+            promptNewTask();
+        }
+        return true;
     }
 
     public static Intent launchIntent(Context context) {
         return new Intent(context, ToDoListActivity.class);
     }
+
+    private void makeNavigationBarWhite() {
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.white));
+    }
+
+    private void makeStatusBarWhite() {
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+
+    private void setActionBar() {
+        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.to_do_list_title);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void promptNewTask() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(ToDoListActivity.this);
+
+        alert.setMessage(R.string.add_task_prompt);
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // add to singleton
+            }
+        });
+
+        alert.setNegativeButton("Cancel", null);
+
+        alert.show();
+    }
+
 }
