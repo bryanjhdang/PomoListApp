694a63a9d9a0229d3c1da58f14fdb97addfc4369
nnn2 <nnn2@sfu.ca>
2021-12-16 16:53:40 -0800

    TimerActivity.java: added 3 buttons listener for the bottom bar.

--- app/src/main/java/app/project/pomolist/TimerActivity.java
+++ app/src/main/java/app/project/pomolist/TimerActivity.java
@@ -1,23 +1,46 @@
 package app.project.pomolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * Activity shows the graphical timer.
  */
-public class TimerActivity extends AppCompatActivity {
+public class TimerActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        BottomNavigationView menuBar = findViewById(R.id.navigation_bar);
+        menuBar.setOnNavigationItemSelectedListener(this);
     }
 
     public static Intent launchIntent(Context context) {
         return new Intent(context, TimerActivity.class);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menu_timer:
+                return true;
+            case R.id.menu_tasks:
+                ToDoListActivity.launchIntent(TimerActivity.this);
+            case R.id.menu_settings:
+                SettingsActivity.launchIntent(TimerActivity.this);
+        }
+
+        return false;
     }
 }
